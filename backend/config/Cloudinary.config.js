74f3c022c97c42ebf96aa8a2ff436105d14fc8fb
anish <anishpanda3@gmail.com>
2025-02-19 23:51:02 +0530

    done

--- backend/config/Cloudinary.config.js
+++ backend/config/Cloudinary.config.js
@@ -1,13 +1,12 @@
+import cloudinary from "cloudinary";
+import dotenv from "dotenv";
 
+dotenv.config(); // Load environment variables
 
-const { v2: cloudinary } = require('cloudinary');
-require('dotenv').config(); 
-
-
-export cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dohajvmjw',
-  api_key: process.env.CLOUDINARY_API_KEY || '549381837364977',
-  api_secret: process.env.CLOUDINARY_API_SECRET || '88YpmyGAVJmqz-fpYO7soQzXlqw',
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
+export default cloudinary.v2; // Export the v2 instance
